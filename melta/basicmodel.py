3b0d8b631ad98153b7ff741394f92529563a94ad
ernesto <bossi.ernestog@gmail.com>
2014-01-15 03:19:13 -0300

    added basic hierarchy to objects

--- melta/basicmodel.py
+++ melta/basicmodel.py
@@ -1,11 +1,18 @@
 from .random import generate_object_id, generate_object_name
 
 
-class AtomicObject(object):
+class MeltaBaseObject(object):
     def __init__(self, melta_instance_name=None, *args, **kwargs):
         self.id = generate_object_id()
-        print(self.generate_name())
         self.instance_name = melta_instance_name or self.generate_name()
+
+    def generate_name(self):
+        return generate_object_name(self.__class__.__name__)
+
+
+class AggregationObject(MeltaBaseObject):
+    def __init__(self, melta_instance_name=None, *args, **kwargs):
+        super(AggregationObject, self).__init__(melta_instance_name=melta_instance_name, *args, **kwargs)
         if kwargs:
             self.set_attrbutes(**kwargs)
 
@@ -13,5 +20,28 @@ class AtomicObject(object):
         for name in kwargs:
             setattr(self, name, kwargs[name])
 
-    def generate_name(self):
-        return generate_object_name(self.__class__.__name__)
+    def __getattribute__(self, name):
+        attr_value = super(AggregationObject, self).__getattribute__(name)
+        return attr_value
+
+
+class AtomicObject(MeltaBaseObject):
+    def __init__(self, melta_instance_name=None, *args, **kwargs):
+        super(AtomicObject, self).__init__(melta_instance_name, *args, **kwargs)
+        self.check_one_argument_restriction(*args)
+        self.assign_arguments(*args)
+
+    def check_one_argument_restriction(self, *arguments):
+        if len(arguments) > 1:
+            raise AtomicObjectInvalidArgumentsException('You can only input a single value to an Atomic Object, trying to assign {0} values'.format(
+                len(arguments)))
+
+    def assign_arguments(self, *arguments):
+        if not arguments:
+            self.value = None
+            return
+        self.value = arguments[0]
+
+
+class AtomicObjectInvalidArgumentsException(Exception):
+    pass
--- test/basicmodeltest.py
+++ test/basicmodeltest.py
@@ -1,12 +1,33 @@
 from unittest import TestCase
-from melta.basicmodel import AtomicObject
+from melta.basicmodel import AggregationObject, AtomicObject
 
 
-class BasicModelTest(TestCase):
+class AtomicModelTest(TestCase):
+    def test_create_valid_atomic_object(self):
+        atomic = AtomicObject(None, 2)
+        self.assertEqual(atomic.value, 2)
+        self.assertTrue(atomic.instance_name.startswith('AtomicObject'))
+
+    def test_create_named_atomic_object(self):
+        atomic = AtomicObject('PersonID', 23)
+        self.assertEqual(atomic.value, 23)
+        self.assertEqual(atomic.instance_name, 'PersonID')
+
+    def test_create_invalid_atomic_object(self):
+        from melta.basicmodel import AtomicObjectInvalidArgumentsException
+
+        self.assertRaises(AtomicObjectInvalidArgumentsException, AtomicObject, 'PersonID', 24, 2)
+
+    def test_create_null_value_object(self):
+        atomic = AtomicObject('PersonID')
+        self.assertEqual(atomic.value, None)
+
+
+class AggregationModelTest(TestCase):
     def setup(self):
         pass
 
-    def test_atomic_object(self):
-        new_atomic_object = AtomicObject(**{'name': 'Mark', 'age': 23})
-        self.assertEqual(new_atomic_object.name,'Mark')
-        self.assertEqual(new_atomic_object.age,23)
+    def test_aggregated_object(self):
+        new_atomic_object = AggregationObject(**{'name': 'Mark', 'age': 23})
+        self.assertEqual(new_atomic_object.name, 'Mark')
+        self.assertEqual(new_atomic_object.age, 23)
